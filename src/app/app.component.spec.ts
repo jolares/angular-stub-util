69937430ab6db3afbe53aaaf187e5920e839b4d3
Jo Lares <jo_lares@ultimatesoftware.com>
2019-08-24 08:44:07 -0400

    refact: Rename project as Jazz

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ng-jazz'`, () => {
+  it(`should have as title 'jazz'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ng-jazz');
+    expect(app.title).toEqual('jazz');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ng-jazz!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to jazz!');
   });
 });
